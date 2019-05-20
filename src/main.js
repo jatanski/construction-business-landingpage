7d365ce1cdfef7657dd0cf90d527e87ed2d81e43
Kuba <ja.tanski@gmail.com>
2019-05-21 00:30:40 +0200

    add [aboutMobile]

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import photo1 from "../public/img/foto1.jpg";
 import photo2 from "../public/img/foto2.jpg";
 import photo3 from "../public/img/foto3.jpg";
 import photo4 from "../public/img/foto4.jpg";
+import photoAbout from "../public/img/foto-about.jpg";
 import "./slider";
 import "./mobileMenu";
 
 const slider = document.querySelector(".header__slider");
 
-slider.children[0].style.backgroundImage = `url(dist/${photo1})`;
-slider.children[1].style.backgroundImage = `url(dist/${photo2})`;
-slider.children[2].style.backgroundImage = `url(dist/${photo3})`;
-slider.children[3].style.backgroundImage = `url(dist/${photo4})`;
+// slider.children[0].style.backgroundImage = `url(dist/${photo1})`;
+// slider.children[1].style.backgroundImage = `url(dist/${photo2})`;
+// slider.children[2].style.backgroundImage = `url(dist/${photo3})`;
+// slider.children[3].style.backgroundImage = `url(dist/${photo4})`;
+
+const aboutImage = document.querySelector(".about__imageContainter-image");
+
+aboutImage.style.backgroundImage = `url(dist/${photoAbout})`;
