db48928f28ac7e400f451cadacff760ae6f058ae
Kuba <ja.tanski@gmail.com>
2019-05-22 17:40:39 +0200

    a lot of change in header & about, add animatedSlider

--- src/main.js
+++ src/main.js
@@ -9,29 +9,47 @@ import lublinMap from "../public/img/lublin-map.jpg";
 import "./slider";
 import "./mobileMenu";
 import lightbox from "./lightbox";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+// Add image to slider in header
 const slider = document.querySelector(".header__slider");
-
 slider.children[0].style.backgroundImage = `url(dist/${photo1})`;
 slider.children[1].style.backgroundImage = `url(dist/${photo2})`;
 slider.children[2].style.backgroundImage = `url(dist/${photo3})`;
 slider.children[3].style.backgroundImage = `url(dist/${photo4})`;
 
+// Add image in about section
 const aboutImage = document.querySelector(".about__imageContainter-image");
-
 aboutImage.style.backgroundImage = `url(dist/${photoAbout})`;
 
+// Add backgroundImage in contact section
 const contactHeader = document.querySelector(".contact__header");
 contactHeader.style.backgroundImage = `url(dist/${contactBackground})`;
 
+// Add map in contact section
 const contactMap = document.querySelector(".contact-map");
 contactMap.style.backgroundImage = `url(dist/${lublinMap})`;
 
+// In start page mainContainer is display:none, beacuse on the screen displays loader section.
+// After that display check to block and show on the screen.
 const mainContainer = document.querySelector(".mainContainer");
 mainContainer.style.display = "none";
 
 const showPage = () => {
   mainContainer.style.display = "block";
 };
+setTimeout(showPage, 1000);
 
-setTimeout(showPage, 4200);
+//Function animation underline in desktop menu elements.
+// const animationUnderline = el => {
+//     el
+// };
+
+// const menuElements = document.querySelectorAll(".link::after");
+// console.log(menuElements);
+// menuElements.forEach(el => )
+
+// AnimateAbout();
+
+AOS.init({ duration: 2000 });
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(scss)$/,
+        test: /\.(scss|css)$/,
         use: [
           {
             loader: "style-loader",
