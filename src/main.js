fd8e5818b962f01ad3ae44e98c422f91c91cedc2
Kuba <ja.tanski@gmail.com>
2019-05-22 10:35:11 +0200

    migration recomend to contact branch

--- src/main.js
+++ src/main.js
@@ -5,20 +5,24 @@ import photo3 from "../public/img/foto3.jpg";
 import photo4 from "../public/img/foto4.jpg";
 import photoAbout from "../public/img/foto-about.jpg";
 import contactBackground from "../public/img/contact-background.jpg";
+import lublinMap from "../public/img/lublin-map.jpg";
 import "./slider";
 import "./mobileMenu";
-import { lightbox } from "./lightbox";
+// import { lightbox } from "./lightbox";
 
 const slider = document.querySelector(".header__slider");
 
-slider.children[0].style.backgroundImage = `url(dist/${photo1})`;
-slider.children[1].style.backgroundImage = `url(dist/${photo2})`;
-slider.children[2].style.backgroundImage = `url(dist/${photo3})`;
-slider.children[3].style.backgroundImage = `url(dist/${photo4})`;
+// slider.children[0].style.backgroundImage = `url(dist/${photo1})`;
+// slider.children[1].style.backgroundImage = `url(dist/${photo2})`;
+// slider.children[2].style.backgroundImage = `url(dist/${photo3})`;
+// slider.children[3].style.backgroundImage = `url(dist/${photo4})`;
 
 const aboutImage = document.querySelector(".about__imageContainter-image");
 
-aboutImage.style.backgroundImage = `url(dist/${photoAbout})`;
+// aboutImage.style.backgroundImage = `url(dist/${photoAbout})`;
 
 const contactHeader = document.querySelector(".contact__header");
 contactHeader.style.backgroundImage = `url(dist/${contactBackground})`;
+
+const contactMap = document.querySelector(".contact-map");
+contactMap.style.backgroundImage = `url(dist/${lublinMap})`;
